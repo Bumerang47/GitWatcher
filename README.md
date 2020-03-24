ae14d152ecf1a0f26ca70dd645f223dc44ed0943
Alex Uliantsev <alex@uliantsev.name>
2020-03-24 13:32:13 +0300

    update desription runbook

--- README.md
+++ README.md
@@ -12,17 +12,24 @@ Before run on Pycharm turn on "*Emulate terminal in output console*"
 
 ### Description
 ```bash
-usage: GitWatcher [-h] [--branch BRANCH] [--since SINCE] [--until UNTIL] url
+usage: git_wathcer [-h] [--branch BRANCH] [--since SINCE] [--until UNTIL] [--no-debug] [--auth AUTH] [--update-interval UPDATE_INTERVAL] [--size-top-table SIZE_TOP_TABLE] URL
+
+Simple dashboard of git repository
 
 positional arguments:
-  url              Repository URL
+  URL                   Repository URL
 
 optional arguments:
-  -h, --help       show this help message and exit
-  --branch BRANCH  Branch name for analyze commits
-  --since SINCE    Get result after this date. This is a timestamp or ISO format
-  --until UNTIL    Get result before this date. This is a timestamp or ISO format
-
+  -h, --help            show this help message and exit
+  --branch BRANCH       branch name for analyze commits [master].
+  --since SINCE         get result after this date. This is a timestamp or ISO format [None]
+  --until UNTIL         get result before this date. This is a timestamp or ISO format [None]
+  --no-debug            disable debug and output to stdout [True].
+  --auth AUTH           authenticate for pass or just get more rate limit. Example: `<login>:<pass>` or `<clent_id>:<clent_secret>`
+  --update-interval UPDATE_INTERVAL
+                        period in second between repeat upload data [600].
+  --size-top-table SIZE_TOP_TABLE
+                        size table of top contributors [30].
 ```
 
 Example URL: `https://github.com/:owner/:repo/`
--- git_watcher/config.py
+++ git_watcher/config.py
@@ -15,24 +15,25 @@ def parse_date(v):
 
 
 def base_parser():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog='git_wathcer',
+                                     description='Simple dashboard of git repository')
     parser.add_argument('dest', type=Destination.from_str, metavar='URL', help='Repository URL')
     parser.add_argument('--branch', type=str, default='master', required=False,
-                        help='Branch name for analyze commits [%(default)s].')
+                        help='branch name for analyze commits [%(default)s].')
     parser.add_argument('--since', type=parse_date, default=None, required=False,
-                        help='Get result after this date. This is a timestamp or ISO format '
+                        help='get result after this date. This is a timestamp or ISO format '
                              '[%(default)s]')
     parser.add_argument('--until', type=parse_date, default=None,
-                        help='Get result before this date. This is a timestamp or ISO format '
+                        help='get result before this date. This is a timestamp or ISO format '
                              '[%(default)s]')
     parser.add_argument('--no-debug', default=True, dest='debug', action='store_false',
-                        help='Disable debug and output to stdout [%(default)s].')
+                        help='disable debug and output to stdout [%(default)s].')
     parser.add_argument('--auth', default='', type=str,
-                        help='Authenticate for pass or just get more rate limit.\n'
+                        help='authenticate for pass or just get more rate limit.\n'
                              'Example: `<login>:<pass>` or `<clent_id>:<clent_secret>`')
     parser.add_argument('--update-interval', default=600, type=int,
-                        help='Period in second between repeat upload data [%(default)s].')
+                        help='period in second between repeat upload data [%(default)s].')
     parser.add_argument('--size-top-table', default=30, type=int,
-                        help='Size table of top contributors [%(default)s].')
+                        help='size table of top contributors [%(default)s].')
     args, _ = parser.parse_known_args()
     return args
